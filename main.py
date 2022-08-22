8274654ad9b3945a78ee6f46fb42a25ca1bd69c7
haoaoju <74438549+haoaoju@users.noreply.github.com>
2022-08-22 17:38:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ birthday = os.environ['BIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID1"]
+user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
