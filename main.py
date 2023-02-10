2be65637e2095015ced14e72101734a2eb8b918c
liangyude007 <58420025+liangyude007@users.noreply.github.com>
2023-02-10 10:30:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "https://restapi.amap.com/v3/weather/weatherInfo?key=548d5a72ba6a78290dad9f64df1d5d14&city=130600"
   res = requests.get(url).json()
   weather = res['lives'][0]
-  return weather['temperature'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temperature'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
