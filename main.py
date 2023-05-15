a8f7fc03a98aaaaac0863a5e814107116d93abd8
liangyude007 <58420025+liangyude007@users.noreply.github.com>
2023-05-15 11:51:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
+from requests.adapters import HTTPAdapter
 import os
 import random
 
@@ -10,27 +11,26 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
-
 weather_key = os.environ["WEATHER_KEY"]
 
+s = requests.Session()
+s.mount('http://', HTTPAdapter(max_retries=3))
+s.mount('https://', HTTPAdapter(max_retries=3))
+
 def get_weather():
-  url = "https://restapi.amap.com/v3/weather/weatherInfo?key=21dac7a0611332385badf4b96449582c&city=130600"
+  url = "https://restapi.amap.com/v3/weather/weatherInfo?key="+weather_key +"&city=130600"
   try:
-    print('获取天气url：',url)
     print('start===天气开始时间：',datetime.now())
-    res = requests.get(url,timeout=20)
+    res = s.get(url,timeout=20)
     print('获取天气结果：',res.json()['lives'][0])
     weather = res.json()['lives'][0]
     return weather['weather'], math.floor(int(weather['temperature']))
   except:
-    print('end===天气结束时间：',datetime.now())
-    print('获取天气出现异常')
+    print('获取天气出现异常===天气结束时间：',datetime.now())
     return '如往常','比较正常'
 
 
@@ -47,12 +47,11 @@ def get_birthday():
 def get_words():
   try:
     print('start===文案开始时间：',datetime.now())
-    words = requests.get("https://api.shadiao.pro/chp",timeout=20)
+    words = s.get("https://api.shadiao.pro/chp",timeout=20)
     print('获取文案JSON：',words.json())
     return words.json()['data']['text']
   except:
-    print('end===文案结束时间：',datetime.now())
-    print('获取文案出现异常')
+    print('获取文案出现异常===文案结束时间：',datetime.now())
     return '一想到你，我这张脸就泛起微笑——梁育德TO陈明'
   
   
