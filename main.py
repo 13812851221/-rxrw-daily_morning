3336dfc5c195e23a739869fcf3068034df1758f6
liangyude007 <58420025+liangyude007@users.noreply.github.com>
2023-05-15 11:33:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,16 @@ template_id = os.environ["TEMPLATE_ID"]
 weather_key = os.environ["WEATHER_KEY"]
 
 def get_weather():
-  url = "https://restapi.amap.com/v3/weather/weatherInfo?key="+weather_key+"&city=130600"
+  url = "https://restapi.amap.com/v3/weather/weatherInfo?key=21dac7a0611332385badf4b96449582c&city=130600"
   try:
     print('获取天气url：',url)
+    print('start===天气开始时间：',datetime.now())
     res = requests.get(url,timeout=20)
     print('获取天气结果：',res.json['lives'][0])
     weather = res.json['lives'][0]
     return weather['weather'], math.floor(int(weather['temperature']))
   except:
+    print('end===天气结束时间：',datetime.now())
     print('获取天气出现异常')
     return '如往常','比较正常'
 
@@ -44,10 +46,12 @@ def get_birthday():
 
 def get_words():
   try:
+    print('start===天气开始时间：',datetime.now())
     words = requests.get("https://api.shadiao.pro/chp",timeout=20)
     print('获取文案JSON：',words.json())
     return words.json()['data']['text']
   except:
+    print('end===天气结束时间：',datetime.now())
     print('获取文案出现异常')
     return '一想到你，我这张脸就泛起微笑——梁育德TO陈明'
   
